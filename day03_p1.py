19cb8b7cf715ea6aa9c37280f62ea43c36b47620
a16thegu <37794246+a16thegu@users.noreply.github.com>
2019-12-10 11:34:35 +0100

    Update day03_p1.py
    All positions for the two wires has been calculated from the test input. Now the Manhattan Distance need to be calculated for the two wires and to be compaired.

--- day03_p1.py
+++ day03_p1.py
@@ -47,12 +47,56 @@
 # U62,R66,U55,R34,D71,R55,D58,R83 = distance 159
 # R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
 # U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = distance 135
+
 # What is the Manhattan distance from the central port to the closest intersection?
+
+import re
+
+def splitInput(wirePath) :
+    wire = []
+    for path in wirePath :
+        match = re.match(r"([a-z]+)([0-9]+)", path, re.I)
+        if match:
+            items = match.groups()
+            wire.append(items)
+
+    return wire
+
+def addWirePosition(path) :
+    wirePos = [(0, 0)]
+    index = 0
+    for pair in path :
+        if pair[0] == 'U' :
+            wirePos.append((wirePos[index][0], wirePos[index][1] + int(pair[1])))
+            index += 1
+        elif pair[0] == 'R' :
+            wirePos.append((wirePos[index][0] + int(pair[1]), wirePos[index][1]))
+            index += 1
+        elif pair[0] == 'D' :
+            wirePos.append((wirePos[index][0], wirePos[index][1] - int(pair[1])))
+            index += 1
+        elif pair[0] == 'L' :
+            wirePos.append((wirePos[index][0] - int(pair[1]), wirePos[index][1]))
+            index += 1
+
+    return wirePos
 
 fname = "input_day03.txt"
 fh = open(fname)
-wire1 = []
-wire2 = []
 
-for line in fh :
-    wire1 += line.split(',')
+#for line in fh :
+
+testWire1 = ['R75', 'D30', 'R83', 'U83', 'L12', 'D49', 'R71', 'U7', 'L72']
+testWire2 = ['U62', 'R66', 'U55', 'R34', 'D71', 'R55', 'D58', 'R83']
+
+wire1 = splitInput(testWire1)
+wire2 = splitInput(testWire2)
+
+posWire1 = addWirePosition(wire1)
+posWire2 = addWirePosition(wire2)
+
+print(posWire1)
+print(posWire2)
+
+
+# For calculating Manhattan Distance: https://www.geeksforgeeks.org/sum-manhattan-distances-pairs-points/
